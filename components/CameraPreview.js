04bd313cc605da70800ab4221cbe0066b59845a5
Marcus Chong <marcus.jn.chong@gmail.com>
2019-04-16 22:24:29 -0400

    store faces in state

--- components/CameraPreview.js
+++ components/CameraPreview.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Camera, Permissions, FaceDetector } from 'expo';
+
 export default class CameraPreview extends React.Component {
   state = {
     hasCameraPermission: null,
     type: Camera.Constants.Type.back,
+    facepos: [],
   };
 
   async componentDidMount() {
@@ -12,8 +14,12 @@ export default class CameraPreview extends React.Component {
     this.setState({ hasCameraPermission: status === 'granted' });
   }
 
-  async handleFacesDetected(e) {
-    console.log(e.faces);
+  async handleFacesDetected (e) {
+    this.setState({
+      facepos: e.faces,
+    })
+
+    console.log(this.state.facepos);
   }
 
   render() {
@@ -24,26 +30,26 @@ export default class CameraPreview extends React.Component {
       return <Text>No access to camera</Text>;
     } else {
       return (
+        
         <View style={{ flex: 1 }}>
+          
           <Camera
             style={{ flex: 1 }}
-            onFacesDetected={this.handleFacesDetected}
+            onFacesDetected={this.handleFacesDetected.bind(this)}
             faceDetectorSettings={{
               mode: FaceDetector.Constants.Mode.fast,
               detectLandmarks: FaceDetector.Constants.Landmarks.all,
               runClassifications: FaceDetector.Constants.Classifications.none,
             }}
             >
+          <View style={{flex: 1, backgroundColor: 'transparent'}}>
+
+            <Text> H </Text>
             
-            <View
-              style={{
-                flex: 1,
-                backgroundColor: 'transparent',
-                flexDirection: 'row',
-              }}>
-            </View>
+          </View>
           </Camera>
         </View>
+
       );
     }
   }
